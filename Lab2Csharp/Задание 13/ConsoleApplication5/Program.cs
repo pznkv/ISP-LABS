13fcbc648ff6356b6c76a239cf382d6275144ef4
pznkv <57153340+pznkv@users.noreply.github.com>
2020-05-12 19:02:25 +0300

    Add files via upload

---
+++ Lab2Csharp/Задание 13/ConsoleApplication5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Random_string
+{
+    class Program
+    {
+        private static Random random;
+        static void Main(string[] args)
+        {
+            random = new Random((int)DateTime.Now.Ticks);
+            string english = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrst" +
+                             "uvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcd" +
+                             "efghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuv";
+            for (int i = 0; i < 30; i++ ){
+                int rad = random.Next(0,english.Length);
+                Console.Write(english[rad]);
+                Console.WriteLine(" Стоит на позиции {0} ", rad);
+            }   
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab2Csharp/Задание 5/ConsoleApplication4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZaglavnieLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stroka;
+            stroka = Convert.ToString(Console.ReadLine());
+            int n = stroka.Length, i;
+            for (i = 0; i < n; i++)
+                if (char.IsUpper(stroka[i]))
+                    if (stroka[i] > 'A' && stroka[i] < 'Z') continue;
+                    else Console.Write(stroka[i]);
+            Console.ReadKey();
+        }
+    }
+}
