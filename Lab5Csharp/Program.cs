9eec9193f640fea370803fb2cd2b50da5dacb068
pznkv <57153340+pznkv@users.noreply.github.com>
2020-05-20 18:41:19 +0300

    Add files via upload

---
+++ Lab5Csharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab5
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Spec studSpec = new Spec("Ян", 17, "Беларусь", (courseNum)1, "ИиТП", "математика, информатика");
+            studSpec.getInfo();
+
+
+        }
+    }
+}
