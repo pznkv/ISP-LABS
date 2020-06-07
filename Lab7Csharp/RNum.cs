19237519eaf64af39ba3e73f002cce9a19bb6e4a
pznkv <57153340+pznkv@users.noreply.github.com>
2020-06-07 18:16:38 +0300

    fully reworked

--- Lab7Csharp/RNum.cs
+++ Lab7Csharp/RNum.cs
@@ -1,145 +1,199 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
-namespace Lab7
+namespace LabRab_7
 {
-    public class RNum : IComparable<T>, IEquatable<RNum>
+    class rational_number : IComparable<rational_number>, IEquatable<rational_number>
     {
-        public int Integer { get;}
-        public int Natural { get;}
+        private readonly long N;
+        private readonly long M;
 
-        public RNum() : this(1, 1)
+        private static long FindGCD(long A, long B)
         {
-        }
-        public RNum(int i, int n)
-        {
-            Integer = i;
-            Natural = n;
-        } 
-
-
-
-        public static RNum operator +(RNum r1, RNum r2)
-        {
-            RNum r = new RNum();
-            r.Integer = r1.Integer * r2.Natural + r2.Integer * r1.Natural;
-            r.Natural = r1.Natural * r2.Natural;
-            return r;
+            A = Math.Abs(A);
+            B = Math.Abs(B);
+            if (A == 0) return B;
+            else if (B == 0) return A;
+            else if (A == B) return A;
+            else if (A == 1 || B == 1) return 1;
+            else if ((A % 2 == 0) && (B % 2 == 0)) return 2 * FindGCD(A / 2, B / 2);
+            else if ((a % 2 == 0) && (b % 2 != 0)) return FindGCD(A / 2, B);
+            else if ((A % 2 != 0) && (B % 2 == 0)) return FindGCD(A, B / 2);
+            else return FindGCD(B, (int)Math.Abs(A - B));
         }
 
-        public static RNum operator -(RNum r1, RNum r2)
-        {
-            RNum r = new RNum();
-            r.Integer = r1.Integer * r2.Natural - r2.Integer * r1.Natural;
-            r.Natural = r1.Natural * r2.Natural;
-            return r;
+        public rational_number(long _n, long _m)
+        {     
+            long GCD = FindGCD(_n, _m);
+            N = _n / GCD;
+            M = _m / GCD;
         }
 
-        public static RNum operator *(RNum r1, RNum r2)
+        public int CompareTo(rational_number Num2)
         {
-            RNum r = new RNum();
-            r.Integer = r1.Integer*r2.Integer;
-            r.Natural = r1.Natural * r2.Natural;
-            return r;
-        }
-
-        public static RNum operator /(RNum r1, RNum r2)
-        {
-            RNum r = new RNum();
-            r.Integer = r1.Integer*r2.Natural;
-            r.Natural = r1.Natural * r2.Integer;
-            return r;
-        }
-
-        public static bool operator >(RNum r1, RNum r2) => ((double)r1.Integer / r2.Natural) > ((double)r2.Integer / r2.Natural);
-        public static bool operator < (RNum r1, RNum r2) => ((double)r1.Integer / r2.Natural) < ((double)r2.Integer / r2.Natural);
-        public static bool operator >= (RNum r1, RNum r2) => ((double)r1.Integer / r2.Natural) >= ((double)r2.Integer / r2.Natural);
-        public static bool operator <= (RNum r1, RNum r2) => ((double)r1.Integer / r2.Natural) <= ((double)r2.Integer / r2.Natural);
-        public static bool operator == (RNum r1, RNum r2) => ((double)r1.Integer / r1.Natural).Equals((double)r2.Integer / r2.Natural);
-        public static bool operator != (RNum r1, RNum r2) => !((double)r1.Integer / r1.Natural).Equals((double)r2.Integer / r2.Natural);
-        public static bool operator !=(RNum r1, int r2) => !((double)r1.Integer / r1.Natural).Equals(r2);
-        public static bool operator ==(RNum r1, int r2) => ((double)r1.Integer / r1.Natural).Equals(r2);
-
-
-        public override bool Equals(object obj) => (double)((RNum)obj).Integer / ((RNum)obj).Natural == (double)this.Integer / this.Natural;
-        public override int GetHashCode() => this.GetHashCode();
-
-        public static implicit operator int (RNum r) => (int)(r.Integer / r.Natural);
-        public static implicit operator double(RNum r) => (double)r.Integer / r.Natural;
-
-        bool IEquatable<RNum>.Equals(RNum other) => (this.Integer == other.Integer && this.Natural == other.Natural);
-        int IComparable.CompareTo(object obj)
-        {
-            if ((this.Integer / this.Natural) > ((RNum)obj).Integer / ((RNum)obj).Natural)
-            {
+            if ((double)this > (double)Num2)
                 return 1;
-            }
-            if ((this.Integer / this.Natural) < ((RNum)obj).Integer / ((RNum)obj).Natural)
-            {
+            else if ((double)this < (double)Num2)
                 return -1;
-            }
             else
-            {
                 return 0;
-            }
         }
 
-        public string RNumStr()
+        public int CompareTo(object other)
         {
-            return $"{(double)Integer / Natural}";
-        }
-        public string FractionStr()
-        {
-            return $"{Integer}/{Natural}";
-        }
-        public string ShowFull()
-        {
-            return "Number: " + RNumStr() + "\nFraction: " + FractionStr() + "\n";
+            if (other is rational_number)
+                return CompareTo((rational_number)other);
+            else
+                throw new InvalidOperationException("CompareTo: not a rational number");
         }
 
-        public static RNum GetFractionFromStr(string str)
+        public override bool Equals(object other)
         {
-            RNum r = new RNum();
-
-            try
+            if(other == null)
             {
-                string[] array = str.Split('/');
-
-                r.Integer = int.Parse(array[0]);
-                r.Natural = int.Parse(array[1]);
+                return false;
             }
-            catch
+            if(other is rational_number)
             {
-                r.Integer = int.Parse(str);
-                r.Natural = 1;
+                return Equals((rational_number)other);
             }
-
-            return r;
+            return false;
         }
 
-        public static RNum GetNumberFromDouble(double a)
+        public bool Equals(rational_number other)
         {
-            RNum r = new RNum();
+            if (other == null)
+                return false;
+            else
+                return (N == other.N && M == other.M);
+        }
 
-            int count = -1;
-            if (a % 10 == 0)
+        public override int GetHashCode()
+        {
+            return (int)(N*17 + M);
+        }
+
+        public static rational_number operator +(rational_number Num1, rational_number Num2)
+        {
+            long MmGCD = FindGCD(Num1.M, Num2.M);
+            long NewN = Num1.N * (Num2.M / MmGCD) + Num2.N * (Num1.M / MmGCD);
+            long NewM = Num1.M * (Num2.M / MmGCD);
+            return new rational_number(NewN, NewM);
+        }
+
+        public static rational_number operator -(rational_number Num1, rational_number Num2)
+        {
+            long MmGCD = FindGCD(Num1.M, Num2.M);
+            long NewN = Num1.N * (Num2.M / MmGCD) - Num2.N * (Num1.M / MmGCD);
+            long NewM = Num1.M * (Num2.M / MmGCD);
+            return new rational_number(NewN, NewM);
+        }
+
+        public static rational_number operator *(rational_number Num1, rational_number Num2)
+        {
+            long NewN = Num1.N * Num2.N;
+            long NewM = Num1.M * Num2.M;
+            return new rational_number(NewN, NewM);
+        }
+
+        public static rational_number operator /(rational_number Num1, rational_number Num2)
+        {
+            long NewN = Num1.N / Num2.N;
+            long NewM = Num1.M / Num2.M;
+            return new rational_number(NewN, NewM);
+        }
+
+        public static bool operator <(rational_number Num1, rational_number Num2)
+        {
+            return Num1.CompareTo(Num2) < 0;
+        }
+
+        public static bool operator >(rational_number Num1, rational_number Num2)
+        {
+            return Num1.CompareTo(Num2) > 0;
+        }
+
+        public static bool operator <=(rational_number Num1, rational_number Num2)
+        {
+            return Num1.CompareTo(Num2) <= 0;
+        }
+
+        public static bool operator >=(rational_number Num1, rational_number Num2)
+        {
+            return Num1.CompareTo(Num2) >= 0;
+        }
+
+        public static explicit operator long(rational_number Num)
+        {
+            return Num.n / Num.m;
+        }
+
+        public static explicit operator double(rational_number Num)
+        {
+            return (double)Num.n / Num.m;
+        }
+
+        // public string RationalToString()
+        // {
+        //     string Str = $"{n}/{m}";
+        //     return Str;
+        // }
+
+        public override string ToString()
+        {
+            string Str = $"{n}/{m}";
+            return Str;   
+        }
+
+        public string RationalToStringDouble()
+        {
+            string Str = $"{(double)n/m)}";
+            return Str;
+        }
+
+        public static rational_number StringToRational(string Str)
+        {
+            string Pattern1 = @"^\d{1,9}\s*\/\s*\d{1,9}$";
+            string Pattern2 = @"^\d{1,9}\s*\.\s*\d{1,9}$";
+            if (Regex.IsMatch(Str, Pattern1))
             {
-                r.Integer = (int)a;
-                r.Natural = 1;
+
+                string[] Numbers = Str.Split(new char[] { ' ', '/' }, StringSplitOptions.RemoveEmptyEntries);
+                long N = long.Parse(Numbers[0]);
+                long M = long.Parse(Numbers[1]);
+                if (M == 0)
+                {
+                    Console.WriteLine("The denominator cannot be equal to zero");
+                    return null;
+                }
+                else
+                {
+                    return new rational_number(N, M);
+                }
+            }
+            else if (Regex.IsMatch(Str, Pattern2))
+            {
+                string[] Numbers = Str.Split(new char[] { ' ', '.' }, StringSplitOptions.RemoveEmptyEntries);
+                long N = long.Parse(Numbers[0] + Numbers[1]);
+                long M = (long)Math.Pow(10, Numbers[1].Length);
+                if (M == 0)
+                {
+                    Console.WriteLine("The denominator cannot be equal to zero");
+                    return null;
+                }
+                else
+                {
+                    return new rational_number(N, M);
+                }
             }
             else
             {
-                for (int i = 0; (float)a % 10 != 0 || Math.Abs(a) < 1; i++)
-                {
-                    a *= 10;
-                    count++;
-                }
-                a /= 10;
-                r.Integer = (int)a;
-                r.Natural = (int)Math.Pow(10, count);
-
+                return null;
             }
-            return r;
         }
-
     }
 }
